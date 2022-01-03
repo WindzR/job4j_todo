d3e27f6b2f33d2e95af0f812c1b4e2aae18057a1
WindzoR <windzr67@gmail.com>
2022-01-03 16:12:15 +0300

    Refactor HbmStore.java with sample Wrapper

--- src/main/java/ru/job4j/store/HbmStore.java
+++ src/main/java/ru/job4j/store/HbmStore.java
@@ -3,6 +3,7 @@ package ru.job4j.store;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -10,6 +11,7 @@ import ru.job4j.entity.Item;
 
 import javax.persistence.Query;
 import java.util.List;
+import java.util.function.Function;
 
 public class HbmStore implements Store, AutoCloseable {
 
@@ -29,87 +31,109 @@ public class HbmStore implements Store, AutoCloseable {
 
     @Override
     public Item add(Item item) {
-        Session session = sf.getCurrentSession();
-        session.beginTransaction();
-        session.saveOrUpdate(item);
-        session.getTransaction().commit();
-        return item;
+        return this.tx(
+            session -> {
+                session.saveOrUpdate(item);
+                return item;
+            }
+        );
     }
 
     @Override
     public boolean replace(int id, Item item) {
-        boolean rsl;
-        Session session = sf.getCurrentSession();
-        session.beginTransaction();
-        Query query = session.createQuery(
+        return this.tx(
+                session -> {
+                    boolean rsl;
+                    final Query query = session.createQuery(
                 "update Item set"
                         + " created = :createdParam,"
                         + " done = :doneParam"
                         + " where id = :idParam");
-        query.setParameter("createdParam", item.getCreated());
-        query.setParameter("doneParam", item.isDone());
-        query.setParameter("idParam", id);
-        int update = query.executeUpdate();
-        session.getTransaction().commit();
-        rsl = update > 0;
-        return rsl;
+                    query.setParameter("createdParam", item.getCreated());
+                    query.setParameter("doneParam", item.isDone());
+                    query.setParameter("idParam", id);
+                    int update = query.executeUpdate();
+                    rsl = update > 0;
+                    return rsl;
+                }
+        );
     }
 
     @Override
     public boolean delete(int id) {
-        boolean rsl;
-        Session session = sf.getCurrentSession();
-        session.beginTransaction();
-        Query query = session.createQuery("delete Item where id = :idParam");
-        query.setParameter("idParam", id);
-        int delete = query.executeUpdate();
-        session.getTransaction().commit();
-        rsl = delete > 0;
-        return rsl;
+        return this.tx(
+                session -> {
+                    boolean rsl;
+                    final Query query = session.createQuery("delete Item where id = :idParam");
+                    query.setParameter("idParam", id);
+                    int delete = query.executeUpdate();
+                    rsl = delete > 0;
+                    return rsl;
+                }
+        );
     }
 
     @Override
     public List<Item> findAll() {
-        List<Item> items = null;
-        Session session = sf.getCurrentSession();
-        session.beginTransaction();
-        items = session.createQuery("from Item")
-                .getResultList();
-        session.getTransaction().commit();
-        return items;
+        return this.tx(
+                session -> {
+                    List<Item> items = null;
+                    items = session.createQuery("from Item")
+                            .getResultList();
+                    return items;
+                }
+        );
     }
 
     @Override
     public List<Item> findAllUnfinished() {
-        List<Item> items = null;
-        Session session = sf.getCurrentSession();
-        session.beginTransaction();
-        items = session.createQuery("from Item where done = false")
-                .getResultList();
-        session.getTransaction().commit();
-        return items;
+        return this.tx(
+                session -> {
+                    List<Item> items = null;
+                    items = session.createQuery("from Item where done = false")
+                            .getResultList();
+                    return items;
+                }
+        );
     }
 
     @Override
     public List<Item> findByName(String name) {
-        List<Item> items = null;
-        Session session = sf.getCurrentSession();
-        session.beginTransaction();
-        Query query = session.createQuery("from Item where name = :nameParam");
-        query.setParameter("nameParam", name);
-        items = query.getResultList();
-        session.getTransaction().commit();
-        return items;
+        return this.tx(
+                session -> {
+                    List<Item> items = null;
+                    final Query query = session.createQuery("from Item where name = :nameParam");
+                    query.setParameter("nameParam", name);
+                    items = query.getResultList();
+                    return items;
+                }
+        );
     }
 
     @Override
     public Item findById(int id) {
-        Item item = null;
+        return this.tx(
+                session -> {
+                    Item item = null;
+                    item = session.get(Item.class, id);
+                    return item;
+                }
+        );
+    }
+
+    private <T> T tx(final Function<Session, T> command) {
         Session session = sf.getCurrentSession();
-        session.beginTransaction();
-        item = session.get(Item.class, id);
-        session.getTransaction().commit();
-        return item;
+        Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception ex) {
+            session.getTransaction().rollback();
+            throw ex;
+        } finally {
+            session.close();
+        }
     }
 
     @Override
